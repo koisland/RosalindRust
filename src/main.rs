1da0711366bd6ac3e54fcc52e8fd4cda864df06b
Keith <koshima789@gmail.com>
2022-03-30 22:36:05 -0700

    Finished rsld_revc.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-
 use std::env;
 
 // mod rsld_dna;
 // use crate::rsld_dna::rsld_dna::cnt_nt;
 
-mod rsld_rna;
-use crate::rsld_rna::rs_rna::replace_u;
+// mod rsld_rna;
+// use crate::rsld_rna::rs_rna::replace_u;
+
+mod rsld_revc;
+use crate::rsld_revc::rs_revc::rev_comp;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let rs_fname = &args[1];
     
     // cnt_nt(rs_fname);
-    replace_u(rs_fname);
+    // replace_u(rs_fname);
+    rev_comp(rs_fname);
 }
