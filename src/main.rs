8763565bd85e203795d7577ba580a0474f5f5185
Keith <koshima789@gmail.com>
2022-03-30 19:53:16 -0700

    Finished rsld_rna

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-mod rsld_dna;
 
 use std::env;
-use crate::rsld_dna::rs_dna::cnt_nt;
+
+// mod rsld_dna;
+// use crate::rsld_dna::rsld_dna::cnt_nt;
+
+mod rsld_rna;
+use crate::rsld_rna::rs_rna::replace_u;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let rs_fname = &args[1];
-    cnt_nt(rs_fname);
+    
+    // cnt_nt(rs_fname);
+    replace_u(rs_fname);
 }
---
+++ src/rsld_rna.rs
@@ -0,0 +1,14 @@
+
+pub mod rs_rna {
+    use std::fs;
+
+    pub fn replace_u(fname: &String) -> String {
+        let contents = fs::read_to_string(fname)
+            .expect("Unable to read file.");
+        
+        let contents_rna = contents.replace("T", "U"); 
+        println!("{}", contents_rna);
+
+        return contents_rna;
+    }
+}
