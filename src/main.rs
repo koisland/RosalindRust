eca9d127cf73cff197fab1ac54959667257bbd15
Keith <koshima789@gmail.com>
2022-03-31 21:29:06 -0700

    Finshed rsld_hamm.

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,11 @@ use std::env;
 // mod rsld_rna;
 // use crate::rsld_rna::rs_rna::replace_u;
 
-mod rsld_revc;
-use crate::rsld_revc::rs_revc::rev_comp;
+// mod rsld_revc;
+// use crate::rsld_revc::rs_revc::rev_comp;
+
+mod rsld_hamm;
+use crate::rsld_hamm::rs_hamm::hamm_dst;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,5 +18,7 @@ fn main() {
     
     // cnt_nt(rs_fname);
     // replace_u(rs_fname);
-    rev_comp(rs_fname);
+    // rev_comp(rs_fname);
+    hamm_dst(rs_fname);
+
 }
---
+++ src/rsld_hamm.rs
@@ -0,0 +1,29 @@
+pub mod rs_hamm {
+    use std::fs;
+
+    pub fn hamm_dst(fname: &String) -> usize {
+        let contents = fs::read_to_string(fname)
+            .expect("Unable to read file.");
+    
+        
+        let split_contents: Vec<&str> = contents
+            .trim()
+            .split("\n")
+            .collect();
+        
+        // needs 2nd trim or length of strings are uneven.
+        let seq_1_len = split_contents[0].trim().len();   
+
+        let mut h_dst: usize = 0;
+        for i in 0..seq_1_len {
+            // Strings in rust are utf-8 and are of variable length. Need to traverse thru entire
+            // string to get length. O(N)
+            if split_contents[0].chars().nth(i).unwrap() != split_contents[1].chars().nth(i).unwrap() {
+                h_dst += 1;
+            }
+        }
+        
+        println!("{}", h_dst);
+        return h_dst;
+    }
+}
