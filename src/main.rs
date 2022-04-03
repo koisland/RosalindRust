b22fe7e6b17a0cedf43fb9674d4442aedb2ef326
Keith <koshima789@gmail.com>
2022-04-02 22:32:22 -0700

    Finished rsld_subs.

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,22 @@ use std::env;
 // mod rsld_revc;
 // use crate::rsld_revc::rs_revc::rev_comp;
 
-mod rsld_hamm;
-use crate::rsld_hamm::rs_hamm::hamm_dst;
+// mod rsld_hamm;
+// use crate::rsld_hamm::rs_hamm::hamm_dst;
+
+mod rsld_subs;
+use crate::rsld_subs::rs_subs::find_motif;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let rs_fname = &args[1];
     
-    // cnt_nt(rs_fname);
-    // replace_u(rs_fname);
-    // rev_comp(rs_fname);
-    hamm_dst(rs_fname);
+    // let res = cnt_nt(rs_fname);
+    // let res = replace_u(rs_fname);
+    // let res = rev_comp(rs_fname);
+    // let res = hamm_dst(rs_fname);
+    let res = find_motif(rs_fname);
+
+    println!("{}", res);
 
 }
